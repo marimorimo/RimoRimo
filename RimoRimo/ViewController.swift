17c808db93ec42fbae1c9df56a5b71d828b5e6d4
wxxd-fxrest <fxrest704@gmail.com>
2024-06-12 17:30:43 +0900

    start

---
+++ RimoRimo/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RimoRimo
+//
+//  Created by 밀가루 on 6/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
