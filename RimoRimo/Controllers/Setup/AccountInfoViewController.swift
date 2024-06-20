62e65672b0d0c53fb8d67ef187e1e867bfd984b8
Yujin Lee <161561452+ljining@users.noreply.github.com>
2024-06-20 18:14:32 +0900

    [Update] 계정 관리 페이지 스크롤X

--- RimoRimo/Controllers/Setup/AccountInfoViewController.swift
+++ RimoRimo/Controllers/Setup/AccountInfoViewController.swift
@@ -24,6 +24,7 @@ class AccountInfoViewController: UIViewController {
         tableView.separatorStyle = .none
         tableView.rowHeight = 58
         tableView.rowHeight = UITableView.automaticDimension
+        tableView.isScrollEnabled = false
         return tableView
     }()
     
