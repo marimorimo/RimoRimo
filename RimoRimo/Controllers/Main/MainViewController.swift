b1a41b03f35d04c4e1d95a92bcc5b15b5f21e05c
wxxd-fxrest <fxrest704@gmail.com>
2024-06-29 21:49:27 +0900

    [Fix] navigation bar hidden

--- RimoRimo/Controllers/Main/MainViewController.swift
+++ RimoRimo/Controllers/Main/MainViewController.swift
@@ -177,7 +177,7 @@ class MainViewController: UIViewController, UNUserNotificationCenterDelegate {
         let timeString = makeTimeString(hour: time.0, min: time.1, sec: time.2)
         stopwatchView.timeLabel.text = timeString
         
-        interval = (targetTimeData ?? 7.0) * 60 // 테스트 시 60 | 배포 시 3600
+        interval = (targetTimeData ?? 7.0) * 3600 // 테스트 시 60 | 배포 시 3600
         
         if value >= Int(interval) {
             stopwatchView.successView.isHidden = false
@@ -286,7 +286,7 @@ class MainViewController: UIViewController, UNUserNotificationCenterDelegate {
     
     // MARK: - Image Timer Connection
     private func calculateCurrentGroup(difference: TimeInterval) -> (currentGroup: Int, totalGroups: Int) {
-        interval = (targetTimeData ?? 7.0) * 60 // 테스트 시 60 | 배포 시 3600
+        interval = (targetTimeData ?? 7.0) * 3600 // 테스트 시 60 | 배포 시 3600
         // print("intervalinterval \(interval)")
         let totalGroups = 5
         let intervalBetweenImages = interval / Double(totalGroups - 1)
